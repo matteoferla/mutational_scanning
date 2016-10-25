d0834799f0ce2f6cc9f4765a420edc3288e55bfb
Matteo Ferla <matteo.ferla@gmail.com>
2016-10-25 23:59:33 +0100

    Somehow this file got a weird tab. Transposon attack!! Fixed now.

--- deep_mut_scanning.py
+++ deep_mut_scanning.py
@@ -21,23 +21,23 @@ import random
 
 def deep_mutation_scan(region, section, target_temp=55, overlap_len=22, primer_range=(None,60), mutation='NNK', GC_bonus=1, Tm_bonus=2.8):
     """
-        Designs primers for quikchange for deep mutation scanning.
-        Based on the overlap principle of http://nar.oxfordjournals.org/content/43/2/e12.long
-        In terms of calculating the melting temperature module is used.
-        For now the calculations are based on everything after the NNK. The problem is the missing thermodynamics for the various mismatches.
-        :param region: the sequence of a region including neighbouring parts and not only the seq of interest.
-        :param section: the range of bases to make primers for as a list/tuple of two items or a slice
-        :param target_temp: the temp threshold. Remember that Phusion HF buffer is secret, but they say the Tm is +3. For salt correction see below
-        :param overlap_len: the length of the overlap of the two primers
-        :param primer_range: the min and max len of the primer.
-        :param mutation: str of the desired mutatated codon
-        :param GC_bonus: 5' GC clamp. This number is added to the Tm when checking if is greater than the set threshold target_temp.
-        :param Tm_bonus: Temp increase due to salt. Distilled water (+0). To match the IDT oligoanalyser with 50 mM Na (+2.8&deg;C). Taq buffer (+4.9&deg;C), Phusion buffer (+11.6&deg;C), Q5 buffer (+13.3&deg;C)
-        :return: a list of dictionaries with the following keys: base codon primer len_homology len_anneal len_primer homology_start homology_stop homology_Tm anneal_Tm
-        
-        Regarding salts. check out mt.salt_correction at http://biopython.org/DIST/docs/api/Bio.SeqUtils.MeltingTemp-module.html#salt_correction
-        if needed.
-        """
+    Designs primers for quikchange for deep mutation scanning.
+    Based on the overlap principle of http://nar.oxfordjournals.org/content/43/2/e12.long
+    In terms of calculating the melting temperature module is used.
+    For now the calculations are based on everything after the NNK. The problem is the missing thermodynamics for the various mismatches.
+    :param region: the sequence of a region including neighbouring parts and not only the seq of interest.
+    :param section: the range of bases to make primers for as a list/tuple of two items or a slice
+    :param target_temp: the temp threshold. Remember that Phusion HF buffer is secret, but they say the Tm is +3. For salt correction see below
+    :param overlap_len: the length of the overlap of the two primers
+    :param primer_range: the min and max len of the primer.
+    :param mutation: str of the desired mutatated codon
+    :param GC_bonus: 5' GC clamp. This number is added to the Tm when checking if is greater than the set threshold target_temp.
+    :param Tm_bonus: Temp increase due to salt. Distilled water (+0). To match the IDT oligoanalyser with 50 mM Na (+2.8&deg;C). Taq buffer (+4.9&deg;C), Phusion buffer (+11.6&deg;C), Q5 buffer (+13.3&deg;C)
+    :return: a list of dictionaries with the following keys: base codon primer len_homology len_anneal len_primer homology_start homology_stop homology_Tm anneal_Tm
+
+    Regarding salts. check out mt.salt_correction at http://biopython.org/DIST/docs/api/Bio.SeqUtils.MeltingTemp-module.html#salt_correction
+    if needed.
+    """
     #
     # sanity
     if isinstance(region,Seq):
@@ -63,29 +63,29 @@ def deep_mutation_scan(region, section, target_temp=55, overlap_len=22, primer_r
         start=x-int(overlap_len/2)
         stop=x+overlap_len-int(overlap_len/2)
         codon = {'codon': region[x:x+3],
-            'AA':str(region[x:x+3].translate())+str(int(x/3)),
-                'base':x,
-                    'homology_start':start,
-                        'homology_stop': stop,
-                            'len_homology': overlap_len,
-                                'homology_Tm': round(mt.Tm_NN(region[start:stop]),1)
-                                }
+                 'AA':str(region[x:x+3].translate())+str(int(x/3)),
+                 'base':x,
+                 'homology_start':start,
+                 'homology_stop': stop,
+                 'len_homology': overlap_len,
+                 'homology_Tm': round(mt.Tm_NN(region[start:stop]),1)
+                 }
         #iterate to find best fw primer.
         for dir in ('fw','rv'):
             for i in range(primer_range[0]-int(overlap_len/2)-3, primer_range[1]-int(overlap_len/2)-3):
                 #the length of the annealing part of the primer is i+3 (the end of the mutated codon)
                 #so the region prior to the mutation does not count: -int(overlap_len/2)-3
-                
+
                 # This cannot be done:
                 #mut=region[start:x]+Seq('NNK')+region[x+3:x+i]
                 #ori=region[start:x+i]
                 #t= mt.Tm_NN(mut, c_seq=ori.complement())
                 # ValueError: no thermodynamic data for neighbors 'GG/TA' available
-                
+
                 #this seems to pick the weakest
                 # mut = region[start:x] + Seq('NNK') + region[x + 3:x + i]
                 # t = mt.Tm_NN(mut)
-                
+
                 # so ignoring forepart
                 if dir == 'fw':
                     mut=region[x+3:x+i]
@@ -93,44 +93,44 @@ def deep_mutation_scan(region, section, target_temp=55, overlap_len=22, primer_r
                     mut = region[x - i:x].reverse_complement()
                 else:
                     raise Exception
-            
+
                 t=mt.Tm_NN(mut)+float(Tm_bonus)
-                
+
                 #check if the tms are good...
                 if mut[-1].upper() in ['C','G'] and t>target_temp-GC_bonus:
                     break
-elif t>target_temp:
-    break
-        else:
-            warn('Target temperature not met. {0}C > {1}C'.format(target_temp,t))
-            
+                elif t>target_temp:
+                    break
+            else:
+                warn('Target temperature not met. {0}C > {1}C'.format(target_temp,t))
+
             '''
-                # check if G:C nearby first.
-                for j in (1,2,3):
+            # check if G:C nearby first.
+            for j in (1,2,3):
                 if mut[-j-1].upper() in ['C','G']:
-                mut2 = region[x + 3:x + i-j]
-                t2 = mt.Tm_NN(mut2)
-                if t2+ 1 > target_temp:
-                t=t2
-                mut=mut2
-                i=i-j
-                '''
+                    mut2 = region[x + 3:x + i-j]
+                    t2 = mt.Tm_NN(mut2)
+                    if t2+ 1 > target_temp:
+                        t=t2
+                        mut=mut2
+                        i=i-j
+            '''
             if dir == 'fw':
                 codon[dir+'_primer']=region[start:x].upper() + mutation.lower() + mut.upper()
-        else: #dir == 'rv'
-            codon[dir + '_primer'] = region[x+3:stop].reverse_complement().upper() + Seq(mutation).reverse_complement().lower() + mut.upper()
+            else: #dir == 'rv'
+                codon[dir + '_primer'] = region[x+3:stop].reverse_complement().upper() + Seq(mutation).reverse_complement().lower() + mut.upper()
             codon[dir+'_len_primer']=len(codon[dir+'_primer'])
             codon[dir+'_anneal_Tm']=round(t,1)
             codon[dir+'_len_anneal']=i
 
         for i in range(primer_range[0] - int(overlap_len / 2) - 3, primer_range[1] - int(overlap_len / 2) - 3):
-            
+
             t = mt.Tm_NN(mut)+float(Tm_bonus)
             if t>target_temp:
                 break
 
-    geneball.append(codon)
-return geneball
+        geneball.append(codon)
+    return geneball
 
 def randomer(n):
     """
