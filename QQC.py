1300c24541c9f997b74e826ff663ca0aa4955b04
Matteo Ferla <matteo.ferla@gmail.com>
2016-10-26 08:37:12 +0100

    Trace class complete. Writing QQC class

--- QQC.py
+++ QQC.py
@@ -26,23 +26,23 @@ class Trace:
 
     @classmethod
     def from_filename(cls, file):
-        '''
+        """
         Class method to return a Trace instance, but from a filename.
         >>> data=Trace.from_filename('sample.ab1')
         :param file: a file name
         :return: Trace instance
-        '''
+        """
         handle = open(file, 'rb')
         record = SeqIO.read(handle, "abi")
         return cls(record)
 
     def find_peak(self, target_seq, strict=True):
-        '''
+        """
         Find the index of the first base of a given sequence in the trace.
         :param target_seq: a string of bases.
         :param strict: boolean, dies if multiple hits.
         :return: the index of peak_id or peak_index of the first base.
-        '''
+        """
         ## sanitise
         target_seq = re.sub('[^ATGC]', '', target_seq.upper())
         targetdex = re.findall(target_seq, self.peak_id)
@@ -56,22 +56,22 @@ class Trace:
         return self.peak_id.find(target_seq)
 
     def find_peak_after(self, target_seq, strict=True):
-        '''
+        """
         Same as find_peak, but give the index of the base after the given sequence.
         :param target_seq: a string of bases.
         :param strict: boolean, dies if multiple hits.
         :return: the index of peak_id or peak_index of the next (last+1) base along
-        '''
+        """
         target_seq = re.sub('[^ATGC]', '', target_seq.upper())
         return self.find_peak(target_seq, strict)+len(target_seq)+1
 
     def get_intensities(self, index,wobble=0.20):
-        '''
+        """
         Get intensities at given peak index.
         :param index: a peak index
         :param wobble: window to look in (default .2)
         :return: dictionary
-        '''
+        """
         span = round(len(self.A)/len(self.peak_index))
         doubleindex=self.peak_index[index]
         return {base: max([getattr(self,base)[doubleindex+i] for i in range(-round(span*wobble), round(span*wobble))]) for base in self.bases}
@@ -80,21 +80,33 @@ class Trace:
         if isinstance(location, str):
             location = self.find_peak_after(location)
             ## go..
-        QQC([self.get_intensities(location)],*args, **kwargs)
+        QQC([self.get_intensities(location),self.get_intensities(location+1),self.get_intensities(location+2)],*args, **kwargs)
 
 
 class QQC:
-    def __init__(self,trace,location, scheme='NNK'):
-        pass
+    def __init__(self,peak_int,scheme='NNK'):
+        #make peakfreqs a fraction of one
+        scheme_pred=[{b:.25 for b in 'ATGC'},{b:.25 for b in 'ATGC'},{'A':0,'T':.50, 'G':.50,'C':0}]
+        codon_peak_freq=[{b:p[b]/sum(p.values()) for b in 'ATGC'} for p in peak_int]
+        codon_dev_pf=[sum([scheme_pred[i][b] - abs(scheme_pred[i][b] - codon_peak_freq[i][b]) for b in 'ATGC']) for i in range(3)]
+        '''
+weights=sum(scheme_pred>0,2)./sum(scheme_pred(:)>0);
+wsum=sum(deviation.*weights);
+worse=sum(scheme_pred-abs(scheme_pred-[ones(3,1), zeros(3,3)]),2);
+wmin=sum(worse.*weights);
+Qpool=(wsum+abs(wmin))/(1+abs(wmin));
+'''
+
+
 
     @staticmethod
     def from_trace(trace, location, *args, **kwargs):
-        '''
+        """
         An alternate route. QQC instance comes from Trace(...).QQC()
         :param trace: Trace instance
         :param location: location of first peak of three
         :return: QQC instance
-        '''
+        """
         if not isinstance(trace, Trace):
             trace = Trace(trace)
         return trace.QQC(location, *args, **kwargs)
@@ -114,8 +126,7 @@ if __name__ == "__main__":
 
     file = "example data/ACE-AA-088-01-55Â°C-BM3-A82_19C-T7-T7minus1.ab1"
     x = Trace.from_filename(file)
-    print(x.peak_index)
-    print(x.get_intensities(x.find_peak_after('CGT GAT TTT')))
+    print(x.QQC('CGT GAT TTT'))
     # To Do...
     # get base frequency at given peak
     # QQC class
